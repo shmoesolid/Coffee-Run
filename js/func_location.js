45927c2f3e27b36cbbb6b469b7c59c5065f9ed25
Shane Koehler <shane.koehler@gmail.com>
2020-05-21 20:45:38 -0500

    added location/query api functionality

---
+++ js/func_location.js
@@ -0,0 +1,76 @@
+/** get our location automatically
+ * 
+ */
+function getLocation() 
+{
+    // geolocation supported, try to get position
+    if (navigator.geolocation) 
+        navigator.geolocation.getCurrentPosition(_positionSucess, _positionError);
+    
+    // geolocation not supported, log and try to get by IP
+    else 
+    {
+        console.log("Geolocation is not supported by this browser.");
+        getLocationByIP();
+    }
+}
+
+/** callback for geolocation sucess
+ * 
+ * @param {object} position 
+ */
+function _positionSucess(position) 
+{
+    // run places query function with lat/lon
+    query_places(position.coords.latitude, position.coords.longitude);
+}
+
+/** callback for geolocation failure
+ * 
+ * @param {object} error 
+ */
+function _positionError(error)
+{
+    // fallback get getting location by IP
+    getLocationByIP();
+
+    // log error in console
+    switch(error.code)
+    {
+        case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            console.log("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.");
+            break;
+    }
+}
+
+/** get location by ip address fallback
+ * 
+ */
+function getLocationByIP()
+{
+    // run ajax call
+    $.ajax({
+
+        dataType: "json",
+        url: "https://ipapi.co/json/",
+        
+        success: function(data)
+        {
+            // DEBUG
+            console.log(data);
+
+            // run places query function with lat/lon
+            query_places(data.latitude, data.longitude);
+        }
+
+    });
+}
